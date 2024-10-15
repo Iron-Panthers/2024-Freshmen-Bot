fed5b636afeb426a51dc3f667ddef48ab6df1676
audrey <audreypjohnson@gmail.com>
2024-10-15 16:24:51 -0700

    add drivebase code + other git stuff

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,207 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import static frc.util.MacUtil.IS_COMP_BOT;
+
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.ClosedLoopOutputType;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModule.SteerRequestType;
+import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants.SteerFeedbackType;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Filesystem;
+import frc.robot.Constants.Drive.Dims;
+import frc.util.CAN;
+import java.nio.file.Path;
+import java.util.List;
+
+@SuppressWarnings("java:S1118")
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+  public static final class Config {
+    // maybe tune PID values?
+    public static final HolonomicPathFollowerConfig PATH_FOLLOWER_CONFIG =
+        new HolonomicPathFollowerConfig(
+            new PIDConstants(20, 0, 0),
+            new PIDConstants(10, 0, 0),
+            Drive.MAX_VELOCITY_METERS_PER_SECOND,
+            Math.sqrt(Math.pow(Dims.TRACKWIDTH_METERS, 2) * 2),
+            new ReplanningConfig());
+
+    /** turn this off before comp. */
+    public static final boolean SHOW_SHUFFLEBOARD_DEBUG_DATA = true;
+
+    /** turn this off! only use on practice eboard testing. */
+    public static final boolean DISABLE_SWERVE_INIT = false;
+
+    /** keep this on for pigeon, disable if absolutely necessary */
+    public static final boolean FLIP_GYROSCOPE = true;
+
+    /** def turn this off unless you are using it, generates in excess of 100k rows for a match. */
+    public static final boolean WRITE_APRILTAG_DATA = false;
+
+    public static final Path APRILTAG_DATA_PATH =
+        Filesystem.getDeployDirectory().toPath().resolve("poseEstimationsAtDistances.csv");
+    public static final double REAL_X = 0.0;
+    public static final double REAL_Y = 0.0;
+  }
+
+  public static final class Drive {
+    public static final int PIGEON_PORT = 0; // placeholder
+    public static final String SWERVE_CANBUS = "rio"; // placeholder
+
+    // max voltage delivered to drivebase
+    // supposedly useful to limit speed for testing
+    public static final double MAX_VOLTAGE = 12.0;
+    // maximum velocity
+    // FIXME measure this value experimentally
+    public static final double MAX_VELOCITY_METERS_PER_SECOND =
+        6380.0 // falcon 500 free speed rpm
+            / 60.0
+            * 0.10033
+            * (1 / 6.12) // mk4i l3 16t falcon drive reduction (sourced from adrian)
+            * Math.PI;
+    // theoretical value
+    // FIXME measure and validate experimentally
+    public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND =
+        MAX_VELOCITY_METERS_PER_SECOND
+            / Math.hypot(Dims.TRACKWIDTH_METERS / 2.0, Dims.WHEELBASE_METERS / 2.0)
+            * .5;
+
+    /** the maximum amount of angular error pid loops will tolerate for rotation */
+    public static final double ANGULAR_ERROR = 1.0;
+    /** the minimum magnitude of the right stick for it to be used as a new rotation angle */
+    public static final double ROTATE_VECTOR_MAGNITUDE = .7;
+
+    public static final class Dims {
+      // FIXME validate with hardware
+      public static final double TRACKWIDTH_METERS =
+          .5207; // 20.5 inches (source: cad) converted to meters
+      public static final double WHEELBASE_METERS = TRACKWIDTH_METERS; // robot is square
+
+      public static final double BUMPER_WIDTH_METERS_X = .9779;
+      public static final double BUMPER_WIDTH_METERS_Y = .8382;
+    }
+
+    /*
+     module layout:
+        |──────
+     |->│#   ##steer motor
+     │  │  ##cancoder
+     │  │##drive motor
+     module number
+
+     steer is always left
+     from corner perspective
+
+     robot visualization:
+    |──────────────────────|
+    │2   10          04   1│
+    │  25              24  │
+    │11     S      D     03│
+    │     D          S     │
+    │                      │
+    │                      │
+    │     S          D     │
+    │       D      S       │
+    │12    |────────|    02│
+    │  26  │        │  27  │
+    │3   13│  batt  │01   4│
+    |──────┴───┬┬───┴──────|
+               ││
+               ││
+               ▼▼
+         software front
+     */
+
+    public static final class Modules {
+      public static final class Params {
+        public static final double WHEEL_RADIUS = 2; // also in INCHES
+        public static final double COUPLING_GEAR_RATIO = 3.125;
+        public static final double DRIVE_GEAR_RATIO = 5.357142857142857;
+        public static final double STEER_GEAR_RATIO = 21.428571428571427;
+        public static final Slot0Configs DRIVE_MOTOR_GAINS =
+            new Slot0Configs().withKP(3).withKI(0).withKD(0).withKS(0.32).withKV(0.11).withKA(0);
+        public static final Slot0Configs STEER_MOTOR_GAINS =
+            new Slot0Configs().withKP(11).withKI(0).withKD(0).withKS(0.4).withKV(0.6).withKA(0);
+        public static final ClosedLoopOutputType DRIVE_CLOSED_LOOP_OUTPUT =
+            ClosedLoopOutputType.Voltage;
+        public static final ClosedLoopOutputType STEER_CLOSED_LOOP_OUTPUT =
+            ClosedLoopOutputType.Voltage;
+        public static final SteerFeedbackType FEEDBACK_SOURCE = SteerFeedbackType.FusedCANcoder;
+        public static final double SPEED_TWELVE_VOLTS = MAX_VELOCITY_METERS_PER_SECOND;
+        public static final double SLIP_CURRENT = 0; // optional, unused rn
+        public static final boolean STEER_MOTOR_INVERTED = true;
+
+        public static final DriveRequestType driveRequestType = DriveRequestType.OpenLoopVoltage;
+        public static final SteerRequestType steerRequestType = SteerRequestType.MotionMagic;
+      }
+
+      public static final class Module1 { // back left
+        public static final int DRIVE_MOTOR = CAN.at(4, "module 1 drive motor");
+        public static final int STEER_MOTOR = CAN.at(3, "module 1 steer motor");
+        public static final int STEER_ENCODER = CAN.at(24, "module 1 steer encoder");
+
+        public static final double STEER_OFFSET =
+            IS_COMP_BOT
+                ? 0.41943359375 // comp bot offset
+                : 0.0595703125; // practice bot offset
+      }
+
+      public static final class Module2 { // back right
+        public static final int DRIVE_MOTOR = CAN.at(11, "module 2 drive motor");
+        public static final int STEER_MOTOR = CAN.at(10, "module 2 steer motor");
+        public static final int STEER_ENCODER = CAN.at(25, "module 2 steer encoder");
+
+        public static final double STEER_OFFSET =
+            IS_COMP_BOT
+                ? -0.39990234375 // comp bot offset
+                : 0.262451171875; // practice bot offset
+      }
+
+      public static final class Module3 { // front right
+        public static final int DRIVE_MOTOR = CAN.at(13, "module 3 drive motor");
+        public static final int STEER_MOTOR = CAN.at(12, "module 3 steer motor");
+        public static final int STEER_ENCODER = CAN.at(26, "module 3 steer encoder");
+
+        public static final double STEER_OFFSET =
+            IS_COMP_BOT
+                ? 0.225341796875 // comp bot offset
+                : -0.20825195312; // practice bot offset
+      }
+
+      public static final class Module4 { // front left
+        public static final int DRIVE_MOTOR = CAN.at(2, "module 4 drive motor");
+        public static final int STEER_MOTOR = CAN.at(1, "module 4 steer motor");
+        public static final int STEER_ENCODER = CAN.at(27, "module 4 steer encoder");
+
+        public static final double STEER_OFFSET =
+            IS_COMP_BOT
+                ? 0.316650390625 // comp bot offset
+                : -0.3564453125 + 180; // practice bot offset
+      }
+    }
+
+    public static final class Setpoints {
+      public static final Translation2d SPEAKER = new Translation2d(0, 5.5);
+
+      public static final int SOURCE_DEGREES = 39;
+      public static final int SPEAKER_DEGREES = 11;
+      public static final int EPSILON = 3;
+    }
+  }
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,316 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.Config;
+import frc.robot.Constants.Drive;
+import frc.robot.Constants.Drive.Setpoints;
+import frc.robot.commands.DefaultDriveCommand;
+import frc.robot.commands.RotateAngleDriveCommand;
+import frc.robot.commands.RotateVectorDriveCommand;
+import frc.robot.commands.RotateVelocityDriveCommand;
+import frc.robot.commands.VibrateHIDCommand;
+import frc.robot.subsystems.DrivebaseSubsystem;
+import frc.util.ControllerUtil;
+import frc.util.Layer;
+import frc.util.MacUtil;
+import frc.util.Util;
+import java.util.Map;
+import java.util.function.DoubleSupplier;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+
+  private final DrivebaseSubsystem drivebaseSubsystem = new DrivebaseSubsystem();
+
+  /** controller 1 */
+  private final CommandXboxController jacob = new CommandXboxController(1);
+  /** controller 1 layer */
+  private final Layer jacobLayer = new Layer(jacob.rightBumper());
+  /** controller 0 */
+  private final CommandXboxController anthony = new CommandXboxController(0);
+  /** controller 0 layer */
+  //   private final Layer anthonyLayer = new Layer(anthony.rightBumper());
+
+  /** the sendable chooser to select which auto to run. */
+  private final SendableChooser<Command> autoSelector;
+
+  private GenericEntry autoDelay;
+
+  private Pose2d desiredPose;
+
+  private final ShuffleboardTab driverView = Shuffleboard.getTab("DriverView");
+
+  /* drive joystick "y" is passed to x because controller is inverted */
+  private final DoubleSupplier translationXSupplier =
+      () -> (-modifyAxis(anthony.getLeftY()) * Drive.MAX_VELOCITY_METERS_PER_SECOND);
+  private final DoubleSupplier translationYSupplier =
+      () -> (-modifyAxis(anthony.getLeftX()) * Drive.MAX_VELOCITY_METERS_PER_SECOND);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Set up the default command for the drivetrain.
+    // The controls are for field-oriented driving:
+    // Left stick Y axis -> forward and backwards movement
+    // Left stick X axis -> left and right movement
+    // Right stick X axis -> rotation
+    drivebaseSubsystem.setDefaultCommand(
+        new DefaultDriveCommand(
+            drivebaseSubsystem,
+            translationXSupplier,
+            translationYSupplier,
+            // anthony.rightBumper(),
+            anthony.leftBumper()));
+
+    // pivotSubsystem.setDefaultCommand(
+    //     new PivotManualCommand(pivotSubsystem, () -> -jacob.getLeftY()));
+
+    // Configure the button bindings
+    configureButtonBindings();
+
+    autoSelector = AutoBuilder.buildAutoChooser();
+
+    SmartDashboard.putBoolean("is comp bot", MacUtil.IS_COMP_BOT);
+    SmartDashboard.putBoolean("show debug data", Config.SHOW_SHUFFLEBOARD_DEBUG_DATA);
+    SmartDashboard.putBoolean("don't init swerve modules", Config.DISABLE_SWERVE_INIT);
+
+    desiredPose = new Pose2d();
+    SmartDashboard.putString(
+        "desired pose",
+        String.format(
+            "(%2f %2f %2f)",
+            desiredPose.getX(), desiredPose.getY(), desiredPose.getRotation().getDegrees()));
+
+    if (Config.SHOW_SHUFFLEBOARD_DEBUG_DATA) {
+      driverView.addDouble("Drivebase Angle Error", () -> drivebaseSubsystem.getAngularError());
+    }
+
+    // Create and put autonomous selector to dashboard
+    setupAutonomousCommands();
+  }
+
+  /**
+   * Use this method to do things as the drivers gain control of the robot. We use it to vibrate the
+   * driver b controller to notice accidental swaps.
+   *
+   * <p>Please use this very, very sparingly. It doesn't exist by default for good reason.
+   */
+  public void containerTeleopInit() {
+    // runs when teleop happens
+    CommandScheduler.getInstance().schedule(new VibrateHIDCommand(jacob.getHID(), 5, .5));
+    // vibrate controller at 27 seconds left
+    CommandScheduler.getInstance()
+        .schedule(
+            new WaitCommand(108)
+                .andThen(
+                    new ParallelCommandGroup(
+                        new VibrateHIDCommand(anthony.getHID(), 3, 0.4),
+                        new VibrateHIDCommand(jacob.getHID(), 3, 0.4))));
+  }
+
+  /**
+   * Use this method to do things as soon as the robot starts being used. We use it to stop doing
+   * things that could be harmful or undesirable during game play--rebooting the network switch is a
+   * good example. Subsystems need to be explicitly wired up to this method.
+   *
+   * <p>Depending on which mode the robot is enabled in, this will either be called before auto or
+   * before teleop, whichever is first.
+   *
+   * <p>Please use this very, very sparingly. It doesn't exist by default for good reason.
+   */
+  public void containerMatchStarting() {
+    // runs when the match starts
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    // vibrate jacob controller when in layer
+    jacobLayer.whenChanged(
+        (enabled) -> {
+          final double power = enabled ? .1 : 0;
+          jacob.getHID().setRumble(RumbleType.kLeftRumble, power);
+          jacob.getHID().setRumble(RumbleType.kRightRumble, power);
+        });
+
+    anthony
+        .start()
+        .onTrue(new InstantCommand(drivebaseSubsystem::zeroGyroscope, drivebaseSubsystem));
+
+    jacob
+        .start()
+        .onTrue(new InstantCommand(drivebaseSubsystem::smartZeroGyroscope, drivebaseSubsystem));
+
+    jacob
+        .a()
+        .onTrue(
+            new RotateAngleDriveCommand(
+                    drivebaseSubsystem,
+                    translationXSupplier,
+                    translationYSupplier,
+                    DriverStation.getAlliance().get().equals(Alliance.Red) ? -40 : 40));
+
+    // SOURCE
+    anthony
+        .y()
+        .onTrue(
+            new RotateAngleDriveCommand(
+                    drivebaseSubsystem,
+                    translationXSupplier,
+                    translationYSupplier,
+                    DriverStation.getAlliance().get().equals(Alliance.Red)
+                        ? -Setpoints.SOURCE_DEGREES
+                        : Setpoints.SOURCE_DEGREES));
+
+    // SPEAKER FROM STAGE
+    anthony
+        .b()
+        .onTrue(
+            new RotateAngleDriveCommand(
+                    drivebaseSubsystem,
+                    translationXSupplier,
+                    translationYSupplier,
+                    DriverStation.getAlliance().get().equals(Alliance.Red)
+                        ? -Setpoints.SPEAKER_DEGREES
+                        : Setpoints.SPEAKER_DEGREES));
+
+    // AMP
+    jacob
+        .b()
+        .onTrue(
+            new RotateAngleDriveCommand(
+                    drivebaseSubsystem,
+                    translationXSupplier,
+                    translationYSupplier,
+                    DriverStation.getAlliance().get().equals(Alliance.Red) ? -90 : 90));
+
+    /*    jacob
+            .a()
+            .onTrue(
+                new RotateAngleDriveCommand(
+                        drivebaseSubsystem,
+                        translationXSupplier,
+                        translationYSupplier,
+                        DriverStation.getAlliance().get().equals(Alliance.Red) ? 90 : -90)
+                    .alongWith(new PivotAngleCommand(pivotSubsystem, 138)) // FIXME idk
+                    .alongWith(new ShooterRampUpCommand(shooterSubsystem, ShooterMode.RAMP_AMP_FRONT)));
+    */
+    
+    DoubleSupplier rotation =
+        exponential(
+            () ->
+                ControllerUtil.deadband(
+                    (anthony.getRightTriggerAxis() + -anthony.getLeftTriggerAxis()), .1),
+            2);
+
+    DoubleSupplier rotationVelocity =
+        () -> -rotation.getAsDouble() * Drive.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND * 0.8;
+
+    new Trigger(() -> Math.abs(rotation.getAsDouble()) > 0)
+        .whileTrue(
+            new RotateVelocityDriveCommand(
+                drivebaseSubsystem,
+                translationXSupplier,
+                translationYSupplier,
+                rotationVelocity,
+                anthony.rightBumper()));
+
+    new Trigger(
+            () ->
+                Util.vectorMagnitude(anthony.getRightY(), anthony.getRightX())
+                    > Drive.ROTATE_VECTOR_MAGNITUDE)
+        .onTrue(
+            new RotateVectorDriveCommand(
+                drivebaseSubsystem,
+                translationXSupplier,
+                translationYSupplier,
+                anthony::getRightY,
+                anthony::getRightX,
+                anthony.rightBumper()));
+  }
+
+  /**
+   * Adds all autonomous routines to the autoSelector, and places the autoSelector on Shuffleboard.
+   */
+  private void setupAutonomousCommands() {
+    driverView.addString("NOTES", () -> "...win?\nor not.").withSize(4, 1).withPosition(7, 2);
+
+    driverView.add("auto selector", autoSelector).withSize(4, 1).withPosition(7, 0);
+
+    autoDelay =
+        driverView
+            .add("auto delay", 0)
+            .withWidget(BuiltInWidgets.kNumberSlider)
+            .withProperties(Map.of("min", 0, "max", 15, "block increment", .1))
+            .withSize(4, 1)
+            .withPosition(7, 1)
+            .getEntry();
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    double delay = autoDelay.getDouble(0);
+    return delay == 0
+        ? autoSelector.getSelected()
+        : new WaitCommand(delay).andThen(autoSelector.getSelected());
+  }
+
+  /**
+   * applies deadband and squares axis
+   *
+   * @param value the axis value to be modified
+   * @return the modified axis values
+   */
+  private static double modifyAxis(double value) {
+    // Deadband
+    value = ControllerUtil.deadband(value, 0.07);
+
+    // Square the axis
+    value = Math.copySign(value * value, value);
+
+    return value;
+  }
+
+  private static DoubleSupplier exponential(DoubleSupplier supplier, double exponential) {
+    return () -> {
+      double val = supplier.getAsDouble();
+      return Math.copySign(Math.pow(val, exponential), val);
+    };
+  }
+}
